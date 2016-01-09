537742f7617656b4c5686363f72443517508c6a4
Mikael Carlsson <rmcarlsson@gmail.com>
2016-01-10 00:05:07 +0100

    * Added time hysteresis to control state * Reformat source to comply with GNU style

--- Test/contr/src/control.c
+++ Test/contr/src/control.c
@@ -13,79 +13,72 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include <time.h>
 
 #include "pid.h"
 #include "pid_atune.h"
 #include "temp.h"
 #include "control.h"
 
-bool
-get_pid_params (double* kp, double* ki, double* kd)
-{
+bool get_pid_params(double* kp, double* ki, double* kd) {
 
-  const char filename[] = "pid.par";
-  char line[100];
-  size_t len = 100;
-  bool ret = false;
-  double d = 0;
-  FILE* fd;
+	const char filename[] = "pid.par";
+	char line[100];
+	size_t len = 100;
+	bool ret = false;
+	double d = 0;
+	FILE* fd;
 
-  fd = fopen (filename, "r");
-  if (fd == NULL)
-    {
-      ret = false;
-      return ret;
-    }
-
-  for (int i = 0; i < 3; i++)
-    {
-      char* ok = fgets (line, len, fd);
-      if (ok == NULL)
-	break;
-      else
-	{
-	  sscanf (line, "%lf", &d);
-	  switch (i)
-	    {
-	    case 0:
-	      *kp = d;
-	      break;
-	    case 1:
-	      *ki = d;
-	      break;
-	    case 2:
-	      *kd = d;
-	      ret = true;
-	      printf("Read valid PID parameters, P=%.4lf, I=%.4lf, d=%.4lf\n", *kp, *ki, *kd);
-	      break;
-	    }
+	fd = fopen(filename, "r");
+	if (fd == NULL) {
+		ret = false;
+		return ret;
 	}
-    }
-  (void) fclose (fd);
 
-  return ret;
+	for (int i = 0; i < 3; i++) {
+		char* ok = fgets(line, len, fd);
+		if (ok == NULL)
+			break;
+		else {
+			sscanf(line, "%lf", &d);
+			switch (i) {
+			case 0:
+				*kp = d;
+				break;
+			case 1:
+				*ki = d;
+				break;
+			case 2:
+				*kd = d;
+				ret = true;
+				printf("Read valid PID parameters, P=%.4lf, I=%.4lf, d=%.4lf\n",
+						*kp, *ki, *kd);
+				break;
+			}
+		}
+	}
+	(void) fclose(fd);
+
+	return ret;
 }
 
-void
-save_pid_params (double kp, double ki, double kd)
-{
+void save_pid_params(double kp, double ki, double kd) {
 
-  char filename[] = "pid.par";
-  FILE* fp;
+	char filename[] = "pid.par";
+	FILE* fp;
 
-  fp = fopen (filename, "w");
-  if (fp == NULL)
-    {
-      perror ("Couldn't open the pid.par.");
-      assert(0);
-    }
-  fprintf (fp, "%.5lf\n", kp);
+	fp = fopen(filename, "w");
+	if (fp == NULL) {
+		perror("Couldn't open the pid.par.");
+		assert(0);
+	}
+	fprintf(fp, "%.5lf\n", kp);
 
-  fprintf (fp, "%.5lf\n", ki);
+	fprintf(fp, "%.5lf\n", ki);
 
-  fprintf (fp, "%.5lf\n", kd);
+	fprintf(fp, "%.5lf\n", kd);
 
-  (void) fclose (fp);
+	(void) fclose(fp);
 
 }
 
@@ -97,89 +90,86 @@ control_state_t control_state = CONTROL_HEATING;
 
 int tuning = 0;
 
-void
-control_set_target(temperature_t t)
-{
-  setpoint = (double)(t);
-  control_state = CONTROL_HEATING;
+time_t on_target_time_mark = 0;
+#define STABLE_TIME (double)(10*1000*1000)
+#define HYST_STABLE_TEMP (temperature_t)(2)
+
+
+void control_set_target(temperature_t t) {
+	setpoint = (double) (t);
+	on_target_time_mark = 0;
+	control_state = CONTROL_HEATING;
+}
+
+control_state_t control_get_state() {
+	return control_state;
 }
 
 
-control_state_t
-control_get_state()
-{
-  return control_state;
-}
+void control_init() {
 
+	double kp = 2, ki = 0.5, kd = 2;
 
+	double aTuneStep = 3;
+	double aTuneNoise = 1;
+	unsigned int aTuneLookBack = 500;
 
+	bool val_ok = get_pid_params(&kp, &ki, &kd);
 
+	if (!val_ok) {
+		tuning = 0;
+		printf("No valid pid parameters found, start tuning\n");
+	} else
+		tuning = 1;
 
-void
-control_init ()
-{
+	PID_ATune(&i, &o);
+	SetNoiseBand(aTuneNoise);
+	SetOutputStep(aTuneStep);
+	SetLookbackSec((int) aTuneLookBack);
 
-  double kp = 2, ki = 0.5, kd = 2;
-
-
-  double aTuneStep = 3;
-  double aTuneNoise = 1;
-  unsigned int aTuneLookBack = 500;
-
-  bool val_ok = get_pid_params (&kp, &ki, &kd);
-
-  if (!val_ok)
-    {
-      tuning = 0;
-      printf ("No valid pid parameters found, start tuning\n");
-    }
-  else
-    tuning = 1;
-
-
-  PID_ATune (&i, &o);
-  SetNoiseBand (aTuneNoise);
-  SetOutputStep (aTuneStep);
-  SetLookbackSec ((int) aTuneLookBack);
-
-  PID (&i, &o, &setpoint, kp, ki, kd, DIRECT);
-  SetSampleTime (5000);
-  SetOutputLimits (0, 100);
-  SetMode (AUTOMATIC);
+	PID(&i, &o, &setpoint, kp, ki, kd, DIRECT);
+	SetSampleTime(5000);
+	SetOutputLimits(0, 100);
+	SetMode(AUTOMATIC);
 
 }
 
-int
-control_exec ()
-{
 
-  i = get_temp ();
+int control_exec() {
 
-  if (tuning == 0)
-    {
-      tuning = Runtime ();
-      if (tuning != 0)
-	{
-	  SetTunings (pidat_GetKp (), pidat_GetKi (), pidat_GetKd ());
-	  save_pid_params (pidat_GetKp (), pidat_GetKi (), pidat_GetKd ());
+	i = get_temp();
+
+	if (tuning == 0) {
+		tuning = Runtime();
+		if (tuning != 0) {
+			SetTunings(pidat_GetKp(), pidat_GetKi(), pidat_GetKd());
+			save_pid_params(pidat_GetKp(), pidat_GetKi(), pidat_GetKd());
+		}
+	} else { //we're done, set the tuning parameters
+
+		Compute();
+
+		if ((control_state == CONTROL_HEATING) && (i >= (setpoint - HYST_STABLE_TEMP)))
+		{
+			time_t now = time(NULL);
+
+			if ( on_target_time_mark == 0)
+				on_target_time_mark = time(NULL);
+
+			if ( difftime(now, on_target_time_mark) > STABLE_TIME )
+				control_state = CONTROL_STABLE;
+		}
+		else
+			on_target_time_mark = 0;
+
 	}
-    }
-  else
-    { //we're done, set the tuning parameters
-      Compute ();
 
-      if(( control_state == CONTROL_HEATING ) && (i >= setpoint))
-	control_state = CONTROL_STABLE;
+	int set = 0;
+	if (o < 0)
+		set = 0;
+	else
+		set = (uint16_t) (o);
 
-
-    }
-
-  int set = 0;
-  if (o < 0)
-    set = 0;
-  else
-    set = (uint16_t) (o);
-
-  return set;
+	return set;
 }
 
--- Test/mash_profile/src/mash_profile.c
+++ Test/mash_profile/src/mash_profile.c
@@ -5,6 +5,7 @@
  *      Author: rmcar
  */
 
+#define _GNU_SOURCE
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -30,16 +31,17 @@
  * Tw = strike water temp
  *
  */
+#define mash_water_volume ((double)(3.9))
+#define grain_weight  ((double)(1.3))
 
-const double r = 2.8;
+const double r = (mash_water_volume/grain_weight);
 const double grain_temp = 20;
 const double tdc = 0.41;
 
 int state;
 
-enum state_t
-{
-  INIT, WAIT_FOR_GRAINS, MASHING, HEATING, DONE
+enum state_t {
+	INIT, WAIT_FOR_GRAINS, MASHING, HEATING, DONE
 };
 
 mash_profile_t mp[MAX_MASH_STEPS];
@@ -48,123 +50,108 @@ int mash_step = 0;
 int nrof_mash_steps = 10;
 time_t step_start = 0;
 
-void
-handle_heating ()
-{
-  if (control_get_state () == CONTROL_STABLE)
-    {
-      state = MASHING;
-      printf ("Heating done, for %s at %.1lf C. Will stay for %.0lf minutes.\n",
-	      mp[mash_step].name, mp[mash_step].temp, mp[mash_step].time);
-    }
-}
-
-void
-handle_done ()
-{
-
-}
-
-void
-handle_mashing ()
-{
-  time_t now = time (NULL);
-
-  if (step_start == 0)
-    step_start = time (NULL);
-
-  double diff_time = difftime (now, step_start);
-  if (diff_time >= (double) (mp[mash_step].time * 60))
-    {
-      step_start = 0;
-
-      mash_step++;
-      if (mash_step >= nrof_mash_steps)
-	{
-	  state = DONE;
-	  printf ("Mash done\n");
-	  da_dump ();
-	  control_set_target (0);
+void handle_heating() {
+	if (control_get_state() == CONTROL_STABLE) {
+		state = MASHING;
+		printf(
+				"Heating done, for %s at %.1lf C. Will stay for %.0lf minutes.\n",
+				mp[mash_step].name, mp[mash_step].temp, mp[mash_step].time);
 	}
-      else
-	{
-	  state = HEATING;
-	  control_set_target (mp[mash_step].temp);
-	  printf ("Starting next step, %s. Heating to %.1lf C\n",
-		  mp[mash_step].name, mp[mash_step].temp);
+}
+
+void handle_done() {
+
+}
+
+void handle_mashing() {
+	time_t now = time(NULL);
+
+	if (step_start == 0)
+		step_start = time(NULL);
+
+	double diff_time = difftime(now, step_start);
+	if (diff_time >= (double) (mp[mash_step].time * 60)) {
+		step_start = 0;
+
+		mash_step++;
+		if (mash_step >= nrof_mash_steps) {
+			state = DONE;
+			printf("Mash done\n");
+			da_dump();
+			control_set_target(0);
+		} else {
+			state = HEATING;
+			control_set_target(mp[mash_step].temp);
+			printf("Starting next step, %s. Heating to %.1lf C\n",
+					mp[mash_step].name, mp[mash_step].temp);
+		}
 	}
-    }
 }
 
-void
-handle_wait_for_grains ()
-{
-  char ch;
-  printf (
-      "Strike water temperature reached, targeting first mash step at %.1lf C. Please add grains.\n",
-      mp[0].temp);
-  printf ("Hit any key when done.\n");
-  read (STDIN_FILENO, &ch, 1);
+void handle_wait_for_grains() {
+	char *line = NULL;
+	size_t size;
 
-  /* Reset temperature control from strike water temperature to first mash step temperature */
-  control_set_target (mp[0].temp);
-  printf ("Starting first mash step, %s, at %.lf C\n", mp[mash_step].name,
-	  mp[mash_step].temp);
-  state = MASHING;
+	/* Reset temperature control from strike water temperature to first mash step temperature */
+	control_set_target(mp[0].temp);
+
+	printf(
+			"Strike water temperature reached, targeting first mash step at %.1lf C. Please add grains.\n",
+			mp[0].temp);
+	printf("Hit any key when done.\n");
+
+	(void) getline(&line, &size, stdin);
+
+	printf("Starting first mash step, %s, at %.lf C\n", mp[mash_step].name,
+			mp[mash_step].temp);
+	state = MASHING;
 
 }
 
-void
-handle_init ()
-{
-  if (control_get_state () == CONTROL_STABLE)
-    state = WAIT_FOR_GRAINS;
+void handle_init() {
+	if (control_get_state() == CONTROL_STABLE)
+		state = WAIT_FOR_GRAINS;
 }
 
-void
-mash_init ()
-{
+void mash_init() {
 
-  memset (&mp[0], 0, (sizeof(mash_profile_t) * MAX_MASH_STEPS));
+	memset(&mp[0], 0, (sizeof(mash_profile_t) * MAX_MASH_STEPS));
 
-  read_mash_profile (&mp[0], &nrof_mash_steps);
+	read_mash_profile(&mp[0], &nrof_mash_steps);
 
-  temperature_t strike_water_temp = (tdc / r) * (mp[0].temp - grain_temp)
-      + mp[0].temp;
+	temperature_t strike_water_temp = (tdc / r) * (mp[0].temp - grain_temp)
+			+ mp[0].temp;
 
-  printf (
-      "Heating to strike water temperature at %.1lf C to hit first mash step, %s, at %.1lf C\n",
-      strike_water_temp, mp[0].name, mp[0].temp);
+	printf(
+			"Heating to strike water temperature at %.1lf C to hit first mash step, %s, at %.1lf C\n",
+			strike_water_temp, mp[0].name, mp[0].temp);
 
-  control_set_target (strike_water_temp);
-  state = INIT;
+	control_set_target(strike_water_temp);
+	state = INIT;
 }
 
-void
-mash_exec ()
-{
+void mash_exec() {
 
-  switch (state)
-    {
-    case INIT:
-      handle_init ();
-      break;
-    case WAIT_FOR_GRAINS:
-      handle_wait_for_grains ();
-      break;
-    case HEATING:
-      handle_heating ();
-      break;
-    case MASHING:
-      handle_mashing ();
-      break;
-    case DONE:
-      handle_done ();
-      break;
-    default:
-      assert(0);
-      break;
-    }
+	switch (state) {
+	case INIT:
+		handle_init();
+		break;
+	case WAIT_FOR_GRAINS:
+		handle_wait_for_grains();
+		break;
+	case HEATING:
+		handle_heating();
+		break;
+	case MASHING:
+		handle_mashing();
+		break;
+	case DONE:
+		handle_done();
+		break;
+	default:
+		assert(0);
+		break;
+	}
 
 }
 
